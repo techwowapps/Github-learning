9be625ff3976e94e30e9e5df3c8040536538fbca
techwowapps <39620122+techwowapps@users.noreply.github.com>
2018-05-25 16:43:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
-include('db.php');
-$barcode=$_POST['barcode'];
-//date_default_timezone_set('Asia/Calcutta');
-//$current_timestamp=date('Y-m-d H:i:s');
-$result=mysqli_query($con,"delete from barcode_entry where barcode = '$barcode'");
-if($result)
-{
-	$json['status']=1;
-}
-else
-{
-	$json['status']=0;
-}
-echo json_encode($json);
+
 ?>
